7fd173cd60624b973ac2ffea0370bebaf3786c2b
Victor Bondarenko <nirasimha@gmail.com>
2022-01-31 13:45:23 -0500

    add products route

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const app = express(); //it's working
+const products = ['Apple', 'Google', 'Pinterest'];
 
 app.get('/', (req, res, next) => {
   res.send("It's working");
-})
+});
+
+app.get('/products', (req, res, next) => {
+  res.send(products);
+});
 
 app.listen(5000, () => {
   console.log("It's started...", new Date());
