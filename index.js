4121b808e13331914dc6b7463b8ede35bb25bab0
Victor Bondarenko <nirasimha@gmail.com>
2022-01-31 13:31:31 -0500

    add app get route

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express(); //it's working
+
+app.get('/', (req, res, next) => {
+  res.send("It's working");
+})
+
+app.listen(5000, () => {
+  console.log("It's started...", new Date());
+});
